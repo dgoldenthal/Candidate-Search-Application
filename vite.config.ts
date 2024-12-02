905ec6a35730ee0410048f06c846e08661e0674e
Dov Goldenthal <dgoldenthal@gmail.com>
2024-12-02 23:03:12 +1300

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,20 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
-    host: true
-  },
-  preview: {
-    port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
-    host: true
-  }
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    plugins: [react()],
+    server: {
+      port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
+      host: true
+    },
+    preview: {
+      port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
+      host: true
+    },
+    define: {
+      'process.env.VITE_GITHUB_TOKEN': JSON.stringify(env.VITE_GITHUB_TOKEN)
+    }
+  };
 });
