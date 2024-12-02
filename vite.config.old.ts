86f1ded787ad3ecc03f41142eb0961d44c1660fc
Dov Goldenthal <dgoldenthal@gmail.com>
2024-12-02 22:14:33 +1300

    updated files

--- vite.config.old.ts
+++ vite.config.old.ts
@@ -2,13 +2,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  envDir: './environment',
   plugins: [react()],
-  server: {
-    port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
+  preview: {
+    port: 10000,
     host: true
   },
-  preview: {
-    port: process.env.PORT ? parseInt(process.env.PORT) : 3000,
+  server: {
     host: true
   }
 });
